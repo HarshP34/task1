7ef3c915c08225d97ee2f8df1f1ce71b3cfb2e17
HarshP34 <hupatel1005@gmail.com>
2022-09-29 13:29:26 +0530

    abc

---
+++ new/dom.js
@@ -0,0 +1,12 @@
+//console.dir(document.URL);
+//console.log(document.title);
+//console.log(document.all[10]);
+//console.log(document.body);
+var headerTitle=document.getElementById("header-title");
+var header=document.getElementById("main-header");
+console.log(headerTitle);
+header.style.borderBottom="solid 5px #000";
+var header2=document.getElementsByClassName("title");
+header2[0].style.fontWeight="bold";
+header2[0].style.color="Green";
+
