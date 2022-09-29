bcec00abe7292503af896633ccdee466adacfc0d
HarshP34 <hupatel1005@gmail.com>
2022-09-29 14:45:12 +0530

    task6

--- new/dom.js
+++ new/dom.js
@@ -1,18 +1,14 @@
-var items=document.getElementsByClassName("list-group-item");
+var seconditem=document.querySelector(".list-group-item:nth-child(2)");
+seconditem.style.background="green";
+var thirditem=document.querySelector(".list-group-item:nth-child(3)");
+thirditem.style.color="white";
+var items=document.querySelectorAll(".list-group-item");
 console.log(items);
-items[2].style.background="Green";
-for(var i=0;i<items.length;i++)
+items[1].style.color="green";
+var odd=document.querySelectorAll(".list-group-item:nth-child(odd)");
+for(var i=0;i<odd.length;i++)
 {
-  items[i].style.fontWeight="bold";
+    odd[i].style.background="green";
 }
-
-
-var li=document.getElementsByTagName("li");
-console.log(li);
-for(var i=0;i<li.length;i++)
-{
-  li[i].style.background="gray";
-}
-
 
 
