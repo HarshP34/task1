e77b2871250d80e125b0f31de40aeb1b4b82ce21
HarshP34 <hupatel1005@gmail.com>
2022-09-29 18:05:34 +0530

    task7

--- new/dom.js
+++ new/dom.js
@@ -1,14 +1,50 @@
-var seconditem=document.querySelector(".list-group-item:nth-child(2)");
-seconditem.style.background="green";
-var thirditem=document.querySelector(".list-group-item:nth-child(3)");
-thirditem.style.color="white";
-var items=document.querySelectorAll(".list-group-item");
-console.log(items);
-items[1].style.color="green";
-var odd=document.querySelectorAll(".list-group-item:nth-child(odd)");
-for(var i=0;i<odd.length;i++)
-{
-    odd[i].style.background="green";
-}
+//create a child
+var itemlist=document.querySelector('#items');
+console.log(itemlist.parentNode);
+//lastchild
+console.log(itemlist.lastChild);
+//lastElementchild
+console.log(itemlist.lastElementChild);
+itemlist.lastElementChild.style.color="red";
+//firstchild
+console.log(itemlist.firstChild);
+//firstelementchild
+console.log(itemlist.firstElementChild);
+itemlist.firstElementChild.style.color="blue";
+itemlist.firstElementChild.textContent="Blue";
+//nextsibling
+console.log(itemlist.nextSibling);
+//nextelementsibling
+console.log(itemlist.nextElementSibling);
+//previoussibling
+console.log(itemlist.previousSibling);
+//previouselementsibling
+console.log(itemlist.previousElementSibling);
+itemlist.previousElementSibling.style.color="green";
+//create element
+//create div
+var newDiv=document.createElement('div');
+//add class
+newDiv.className='hello';
+//add id
+newDiv.id='hello1';
+// set attributes
+newDiv.setAttribute('title','Hello Div');
+//create a textnode
+var newDivtext=document.createTextNode('Hello World');
+// add text a div
+newDiv.appendChild(newDivtext);
+console.log(newDiv);
+
+var container=document.querySelector('header .container');
+var h1=document.querySelector('header h1');
+container.insertBefore(newDiv,h1);
+var a=document.querySelector('body .list-group');
+var b=document.querySelector('body .list-group-item');
+a.insertBefore(newDiv,b);
+newDiv.style.color="black";
+
+
+
 
 
