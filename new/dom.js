56ded02ffa6972de272e062621759b3af9ab8a78
HarshP34 <hupatel1005@gmail.com>
2022-09-29 14:06:21 +0530

    task5

--- new/dom.js
+++ new/dom.js
@@ -1,9 +1,18 @@
-
 var items=document.getElementsByClassName("list-group-item");
 console.log(items);
 items[2].style.background="Green";
 for(var i=0;i<items.length;i++)
 {
-    items[i].style.fontWeight="bold";
+  items[i].style.fontWeight="bold";
 }
+
+
+var li=document.getElementsByTagName("li");
+console.log(li);
+for(var i=0;i<li.length;i++)
+{
+  li[i].style.background="gray";
+}
+
+
 
