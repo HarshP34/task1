971a1c4524b25301d246022811f8279b980b00a6
HarshP34 <hupatel1005@gmail.com>
2022-09-30 11:18:59 +0530

    task9

--- new/dom.js
+++ new/dom.js
@@ -1,21 +1,25 @@
 var form =document.getElementById('addForm');
 var itemlist=document.getElementById('items');
+var filter=document.getElementById('filter');
 
 //form submit event
 form.addEventListener('submit',additem);
 //delete event
 itemlist.addEventListener('click',removeitem);
+//filter event
+filter.addEventListener('keyup',filteritems);
 //add item
 function additem(e)
 {
     e.preventDefault();
     //get input value
-    var newitem=document.getElementById('item').value;
+    var newitem=document.querySelectorAll('#item')[0].value;
+    var newitem1=document.querySelectorAll('#item')[1].value;
     //create new li element
     var li=document.createElement('li');
     li.className='list-group-item'; 
    //add textnode 
-   li.appendChild(document.createTextNode(newitem));
+   li.appendChild(document.createTextNode(`${newitem} ${newitem1}`));
    //create delete button
    var deleteBtn=document.createElement('button');
    //add classes
@@ -29,7 +33,6 @@ function additem(e)
    editBtn.appendChild(document.createTextNode('edit'));
    li.appendChild(editBtn);
    itemlist.appendChild(li);
-
 }
 function removeitem(e)
 {
@@ -42,6 +45,21 @@ function removeitem(e)
         }
     }
 }
-
+//filter items
+function filteritems(e)
+{
+    var text=e.target.value.toLowerCase();
+    var items=itemlist.getElementsByTagName('li');
+    Array.from(items).forEach(function(item){
+        var itemname=item.firstChild.textContent;
+        if(itemname.toLowerCase().indexOf(text)!=-1)
+        {
+            item.style.display='block';
+        }
+        else{
+            item.style.display='none';
+        }
+    })
+}
 
 
